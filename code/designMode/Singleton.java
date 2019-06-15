9e19795418d30745654b7b96063b0dc437eb6a5c
zhang1027 <kes6863haobao@163.com>
2019-06-15 19:07:30 +0800

    创建文件夹

---
+++ code/designMode/Singleton.java
@@ -0,0 +1,31 @@
+package com.zhangyu.oneTest.code.designMode;
+
+
+public class Singleton {
+//    private enum SingletonEnum{
+//        INSTANCE;
+//        private Singleton singleton = null;
+//        private SingletonEnum(){
+//            singleton = new Singleton();
+//        }
+//        private Singleton getInstance(){
+//            return singleton;
+//        }
+//    }
+//    public static Singleton getInstance(){
+//        return SingletonEnum.INSTANCE.getInstance();
+//    }
+
+    private static volatile Singleton singleton = null;
+    private Singleton(){}
+    public static Singleton getInstance(){
+        if(singleton ==null){
+            synchronized(Singleton.class){
+                if (singleton == null){
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+}
